ff617574c4c291c762270e9f0d3c098ccccc9717
samet kemal özdemir <samet.ozdemir@ceng.deu.edu.tr>
2022-06-20 18:27:42 +0300

    s

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-s
+const express =require('express');
+const path = require('path');
+
+
+const app =express();
+
+//middleware
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'temp/index.html'));
+})
+
+const port=3000;
+app.listen(port,()=>{
+    console.log(`server starter in port ${port}`);
+});
